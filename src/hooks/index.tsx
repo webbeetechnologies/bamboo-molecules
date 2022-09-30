0ff9b4126daa6abdd2b75eef373caf513b2aaead
Thet Aung <thetaung.dev@gmail.com>
2022-10-01 03:36:23 +0630

    created useCurrentTheme hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,8 +1,11 @@
-import { useTheme as useThemeAtoms } from '@webbee/bamboo-atoms';
+import { useTheme as useThemeAtoms, useColorMode as useColorModeAtoms } from '@webbee/bamboo-atoms';
 import type { ITheme } from '../core';
 
 export { default as useMolecules } from './useMolecules';
 export { default as usePlatformType } from './usePlatformType';
 export { default as useComponentTheme } from './useComponentTheme';
+export { default as useCurrentTheme } from './useCurrentTheme';
 
 export const useTheme: <T extends ITheme>() => T = useThemeAtoms;
+
+export const useColorMode = useColorModeAtoms;
