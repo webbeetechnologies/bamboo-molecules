de84a3d30c9681872556e2af4c3caea9cea8dedf
Thet Aung <thetaung.dev@gmail.com>
2022-10-01 03:35:19 +0630

    updated normalizeStyles func to accept array of style obj

--- src/utils/normalizeStyles.ts
+++ src/utils/normalizeStyles.ts
@@ -4,24 +4,31 @@ import memoize from 'lodash.memoize';
 import type { MD3Theme } from '../core/theme/types';
 
 // normalize tokens inside the styles object and the subsequent objects inside it
-const normalizeStyles = memoize((styles: StyleProp<any>, currentTheme: MD3Theme) => {
-    const newStyles = Object.assign({}, styles);
-    const normalizableProperties = Object.keys(newStyles).filter(
-        key =>
-            (typeof newStyles[key] === 'string' && newStyles[key].includes('.')) ||
-            typeof newStyles[key] === 'object',
-    );
-
-    normalizableProperties.forEach(key => {
-        if (typeof newStyles[key] === 'string') {
-            newStyles[key] = get(currentTheme, newStyles[key], '');
-        } else {
-            // it's an object // we want to normalize everything inside it as well
-            newStyles[key] = normalizeStyles(newStyles[key], currentTheme);
+const normalizeStyles: StyleProp<any> | StyleProp<any>[] = memoize(
+    (styles: StyleProp<any> | StyleProp<any>[], currentTheme: MD3Theme) => {
+        // if the styles is an array, we want to normalize each entries
+        if (Array.isArray(styles)) {
+            return styles.map(styleObj => normalizeStyles(styleObj, currentTheme));
         }
-    });
 
-    return newStyles;
-});
+        const newStyles = Object.assign({}, styles);
+        const normalizableProperties = Object.keys(newStyles).filter(
+            key =>
+                (typeof newStyles[key] === 'string' && newStyles[key].includes('.')) ||
+                typeof newStyles[key] === 'object',
+        );
+
+        normalizableProperties.forEach(key => {
+            if (typeof newStyles[key] === 'string') {
+                newStyles[key] = get(currentTheme, newStyles[key], '');
+            } else {
+                // it's an object // we want to normalize everything inside it as well
+                newStyles[key] = normalizeStyles(newStyles[key], currentTheme);
+            }
+        });
+
+        return newStyles;
+    },
+);
 
 export default normalizeStyles;
