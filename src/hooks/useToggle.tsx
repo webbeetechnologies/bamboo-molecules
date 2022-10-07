f4aeb5b7f742a7b33cd927dadf32788da8e9ab91
Thet Aung <thetaung.dev@gmail.com>
2022-10-07 18:48:17 +0630

    memozied the reuturn value of useToggle hook

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,11 +1,11 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 const useToggle = (initialState = false): [boolean, () => void] => {
     const [state, setState] = useState<boolean>(initialState);
 
     const toggle = useCallback(() => setState(prevState => !prevState), []);
 
-    return [state, toggle];
+    return useMemo(() => [state, toggle], [state, toggle]);
 };
 
 export default useToggle;
