aff91c2cc33711cc630c7cff2d0b6c1e4521a7f4
Thet Aung <thetaung.dev@gmail.com>
2022-11-02 11:45:39 +0630

    created numberinput story

---
+++ example/stories/components/NumberInput/NumberInput.stories.tsx
@@ -0,0 +1,56 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { NumberInputMasks, ProvideMolecules } from 'bamboo-molecules';
+
+import { Example } from './NumberInput';
+
+export default {
+    title: 'components/NumberInput',
+    component: Example,
+    decorators: [
+        Story => (
+            <ProvideMolecules>
+                <Story />
+            </ProvideMolecules>
+        ),
+    ],
+    argTypes: {},
+} as ComponentMeta<typeof Example>;
+
+export const Default: ComponentStory<typeof Example> = args => <Example {...args} />;
+
+Default.args = {
+    placeholder: 'Placeholder',
+    label: 'Enter numbers',
+};
+
+Default.parameters = {
+    docs: {
+        source: {
+            code: `
+<NumberInput placeholder="Placeholder" value={number} onChangeText={masked => setNumber(masked)} {...props}  />
+`,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
+
+export const WithCustomMusk: ComponentStory<typeof Example> = args => <Example {...args} />;
+
+WithCustomMusk.args = {
+    placeholder: 'Placeholder',
+    label: 'Credit card',
+    mask: NumberInputMasks.CREDIT_CARD,
+};
+
+WithCustomMusk.parameters = {
+    docs: {
+        source: {
+            code: `
+<NumberInput placeholder="Placeholder" value={number} onChangeText={masked => setNumber(masked)} {...props} />
+`,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
