9ef76dc0cfa1b62d4a1202ac1b5b6c5a2e6ac4bb
Thet Aung <maxwell@Thets-MacBook-Pro.local>
2022-11-10 17:04:47 +0630

    created useLatest hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -9,5 +9,6 @@ export { default as useCurrentTheme } from './useCurrentTheme';
 export { default as useToggle } from './useToggle';
 export { default as useControlledValue } from './useControlledValue';
 export { default as useFilePicker } from './useFilePicker';
+export { default as useLatest } from './useLatest';
 
 export const useTheme: <T extends ITheme>() => T = useThemeAtoms;
