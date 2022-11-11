a22a6271a0daa44864d303056d19da258b312118
Shashank Agarwal <shashank1010@gmail.com>
2022-11-11 08:29:27 +0300

    Code cleanup

--- sandbox/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import App from "./src/App";
-
-export default () => (
-    <App />
-);
--- sandbox/src/DataSource/PaginatedDataSource/types.ts
+++
@@ -1,2 +0,0 @@
-export type TSetPageAction = (args: {page: number}) => void
-export type setPageSize = (args: {pageSize: number}) => void
