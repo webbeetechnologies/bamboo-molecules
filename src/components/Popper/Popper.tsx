80d19bd9aadff59d1fb857d2264d7a7b9c7fca72
Shashank Agarwal <shashank1010@gmail.com>
2022-11-14 11:55:35 +0300

    Memoized Popper

--- src/components/Popper/Popper.tsx
+++ src/components/Popper/Popper.tsx
@@ -1,10 +1,10 @@
 import { PopperContext } from './PopperContext';
 
-import type { FC, PropsWithChildren } from 'react';
+import { FC, memo, PropsWithChildren } from 'react';
 import type { PopperProps } from './types';
 
 const Popper: FC<PropsWithChildren<PopperProps>> = ({ children, ...props }) => {
     return <PopperContext.Provider value={props} children={children} />;
 };
 
-export default Popper;
+export default memo(Popper);
