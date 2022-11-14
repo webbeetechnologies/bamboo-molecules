752f619fbaebf0055c806b2ed41a0ff4c9839c9a
Shashank Agarwal <shashank1010@gmail.com>
2022-11-14 11:44:19 +0300

    Memoization in Overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,63 +1,62 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import {OverlayContainer} from '@react-native-aria/overlays';
-import {memo, useState} from 'react';
-import {Platform,} from 'react-native';
-import {Modal} from 'react-native';
-import {useKeyboardDismissable} from '../../hooks';
-import {ExitAnimationContext} from '../Animations';
-import type {OverlayProps} from './types';
+import { OverlayContainer } from '@react-native-aria/overlays';
+import { memo, useMemo, useState } from 'react';
+import { Platform } from 'react-native';
+import { Modal } from 'react-native';
+import { useKeyboardDismissable } from '../../hooks';
+import { ExitAnimationContext } from '../Animations';
+import type { OverlayProps } from './types';
 
 const Overlay = ({
-                     isOpen,
-                     children,
-                     onRequestClose,
-                     animationPreset = 'fade',
-                     useRNModalOnAndroid = false,
-                     isKeyboardDismissable = true,
-                     style,
-                 }: OverlayProps) => {
+    isOpen,
+    children,
+    onRequestClose,
+    animationPreset = 'fade',
+    useRNModalOnAndroid = false,
+    isKeyboardDismissable = true,
+    style,
+}: OverlayProps) => {
     const [exited, setExited] = useState(!isOpen);
-
 
     useKeyboardDismissable({
         enabled: isOpen && isKeyboardDismissable,
-        callback: onRequestClose ? onRequestClose : () => {
-        },
+        callback: onRequestClose ? onRequestClose : () => {},
     });
 
+    const value = useMemo(() => {
+        return { exited, setExited };
+    }, [exited, setExited]);
 
-    const styleObj = {...style};
-    if (animationPreset === 'slide') {
-        styleObj.overflow = 'hidden';
-        styleObj.display = 'flex';
-    } else {
-        styleObj.display = exited && !isOpen ? 'none' : 'flex';
-    }
+    const styleObj = useMemo(() => {
+        const styleObj = { ...style };
+        if (animationPreset === 'slide') {
+            styleObj.overflow = 'hidden';
+            styleObj.display = 'flex';
+        } else {
+            styleObj.display = exited && !isOpen ? 'none' : 'flex';
+        }
+
+        return style;
+    }, [style, animationPreset]);
 
     if (Platform.OS === 'android' && useRNModalOnAndroid) {
         return (
-            <ExitAnimationContext.Provider value={{exited, setExited}}>
+            <ExitAnimationContext.Provider value={value}>
                 <Modal
                     transparent
                     visible={isOpen}
                     onRequestClose={onRequestClose}
-                    animationType={animationPreset}
-                >
+                    animationType={animationPreset}>
                     {children}
                 </Modal>
             </ExitAnimationContext.Provider>
         );
     }
 
-
     return (
-        <ExitAnimationContext.Provider value={{exited, setExited}}>
-            <OverlayContainer style={{...styleObj}}>
-                {children}
-            </OverlayContainer>
+        <ExitAnimationContext.Provider value={value}>
+            <OverlayContainer style={styleObj}>{children}</OverlayContainer>
         </ExitAnimationContext.Provider>
     );
-}
-
+};
 
 export default memo(Overlay);
