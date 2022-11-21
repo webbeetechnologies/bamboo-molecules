fa9f1f09524acbcbc3fc0c57f8c9c2c691353b69
Thet Aung <thetaung.dev@gmail.com>
2022-11-21 14:03:53 +0630

    crypto issue on mobile fix

--- hackathon/index.js
+++ hackathon/index.js
@@ -1,8 +1,14 @@
 import { registerRootComponent } from 'expo';
 import 'react-native-gesture-handler';
-import 'react-native-get-random-values';
 
 import App from './App';
+
+// workaround for cypto no defined on mobile
+if (__DEV__ && typeof global.crypto !== 'object') {
+    global.crypto = {
+        getRandomValues: array => array.map(() => Math.floor(Math.random() * 256)),
+    };
+}
 
 // // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // // It also ensures that whether you load the app in Expo Go or in a native build,
