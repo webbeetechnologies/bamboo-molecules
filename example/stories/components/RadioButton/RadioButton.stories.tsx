51cf146dfdf13af966210fb98651dbf2f6d7a78c
Thet Aung <thetaung.dev@gmail.com>
2022-11-28 01:59:41 +0630

    created radiobutton stories

---
+++ example/stories/components/RadioButton/RadioButton.stories.tsx
@@ -0,0 +1,80 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Example, ExampleRadioItem, ExampleWithRadioGroup } from './RadioButton';
+
+export default {
+    title: 'components/RadioButton',
+    component: Example,
+} as ComponentMeta<typeof Example>;
+
+export const Default: ComponentStory<typeof Example> = args => <Example {...args} />;
+
+Default.args = {
+    value: 'example',
+};
+
+Default.parameters = {
+    docs: {
+        source: {
+            code: `
+<RadioButton {...rest} />
+`,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
+
+export const RadioItem: ComponentStory<typeof ExampleRadioItem> = args => (
+    <ExampleRadioItem {...args} />
+);
+
+RadioItem.args = {
+    value: 'item',
+    label: 'Radio Item Label',
+};
+
+RadioItem.parameters = {
+    docs: {
+        source: {
+            code: `
+<RadioButton.Item value="item" label="Radio Item Label" {...rest} />
+`,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
+
+export const WithRadioGroup: ComponentStory<typeof ExampleWithRadioGroup> = args => (
+    <ExampleWithRadioGroup {...args} />
+);
+
+WithRadioGroup.args = {
+    children: (
+        <>
+            <ExampleRadioItem value="first" label="First Item" />
+            <ExampleRadioItem value="second" label="Second Item" />
+        </>
+    ),
+};
+
+WithRadioGroup.parameters = {
+    docs: {
+        source: {
+            code: `
+    const { RadioButton } = useMolecules();
+    const [value, onValueChange] = useState('');
+    
+    return (
+        <RadioButton.Group onValueChange={onValueChange} value={value} {...rest}>
+            <RadioButton.Item value="first" label="First Item" />
+            <RadioButton.Item value="second" label="Second Item" />
+        </RadioButton.Group>
+    )
+`,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
