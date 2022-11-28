ee2bed78081faa08e122c39ee3f0c722cb91dc66
Thet Aung <thetaung.dev@gmail.com>
2022-11-28 20:30:12 +0630

    NumberRangInput stories updated

--- example/stories/components/NumberRangeInput/NumberRangeInput.tsx
+++ example/stories/components/NumberRangeInput/NumberRangeInput.tsx
@@ -7,5 +7,5 @@ export const Example = (props: Props) => {
     const { NumberRangeInput } = useMolecules();
     const [value, setValue] = useState({ min: '', max: '' });
 
-    return <NumberRangeInput {...props} min={value.min} max={value.max} onChange={setValue} />;
+    return <NumberRangeInput min={value.min} max={value.max} onChange={setValue} {...props} />;
 };
