927ba831ea1fcb3f20fe5fb99b78cafbdedeb810
Shashank Agarwal <shashank1010@gmail.com>
2022-12-22 09:31:40 +0300

    Better loading state demo

--- sandbox/src/components/LoadingStatus.tsx
+++ sandbox/src/components/LoadingStatus.tsx
@@ -4,13 +4,36 @@ export const LoadingStatus = () => {
     const ds = useDataSource();
     if (!ds.isLoadable) return null;
 
-    if (ds.isLoading) return <h1>Loading</h1>;
+    if (ds.isLoading)
+        return (
+            <div>
+                <h1 style={{ zIndex: 10, position: 'relative' }}>Loading</h1>
+                <div
+                    style={{
+                        zIndex: 9,
+                        position: 'absolute',
+                        backgroundColor: '#fff',
+                        opacity: 0.7,
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                    }}
+                />
+            </div>
+        );
 
     if (ds.hasLoaded) {
         return <h1>Ready</h1>;
     }
 
-    if (ds.hasErrored) return <h1>Errored</h1>;
+    if (ds.hasErrored)
+        return (
+            <div style={{ display: 'flex', alignItems: 'center', gap: 16 }}>
+                <h1>Errored</h1>
+                <button onClick={ds.fetchRecords}>try again</button>
+            </div>
+        );
 
     return <h1>Not Started</h1>;
 };
