cfee698dfbfb1fdc6c72e8854b68de9ec40d177c
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2022-12-22 16:42:31 +0300

    added numberrangeinput tests

--- example/stories/components/NumberRangeInput/NumberRangeInput.stories.tsx
+++ example/stories/components/NumberRangeInput/NumberRangeInput.stories.tsx
@@ -1,6 +1,9 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 import { Example, ControlledExample } from './NumberRangeInput';
+import { delay } from '../../common';
 
 export default {
     title: 'components/NumberRangeInput',
@@ -45,4 +48,49 @@ Controlled.parameters = {
             type: 'auto',
         },
     },
+};
+
+export const Interactions = Default.bind({});
+
+Interactions.args = {
+    ...Controlled.args,
+    minInputProps: {
+        testID: 'numberRangeInput-min',
+    },
+    maxInputProps: {
+        testID: 'numberRangeInput-max',
+    },
+};
+
+Interactions.parameters = {
+    ...Default.parameters,
+};
+
+Interactions.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await delay(500);
+
+    // type min
+    await userEvent.type(canvas.getByTestId('numberRangeInput-min-flat'), '88');
+
+    await delay(500);
+
+    // type max
+    await userEvent.type(canvas.getByTestId('numberRangeInput-max-flat'), '10');
+
+    await delay(500);
+
+    // min is higher number so the error should show up
+    await expect(canvas.getByText('Invalid number range.')).toBeInTheDocument();
+
+    await delay(500);
+
+    // increase max number
+    await userEvent.type(canvas.getByTestId('numberRangeInput-max-flat'), '0');
+
+    await delay(500);
+
+    // now there shouldn't be any errors
+    await expect(canvas.queryByText('Invalid number range.')).not.toBeTruthy();
 };
