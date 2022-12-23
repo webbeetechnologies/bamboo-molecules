ddc387cbc912e03e98d9ca0c311f0dc87c70e112
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2022-12-23 14:10:52 +0300

    added button interaction tests

--- example/stories/components/Button/Button.stories.tsx
+++ example/stories/components/Button/Button.stories.tsx
@@ -1,5 +1,8 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
+import { delay } from '../../common';
 import { Example } from './Button';
 
 export default {
@@ -12,6 +15,7 @@ export const Text: ComponentStory<typeof Example> = args => <Example {...args} /
 Text.args = {
     children: 'Text Button',
     variant: 'text',
+    testID: 'button-text',
 };
 
 Text.parameters = {
@@ -125,3 +129,79 @@ ButtonPerformance.parameters = {
         allowedGroups: ['client'],
     },
 };
+
+// interaction tests
+
+const hoverInteractions = async ({
+    canvasElement,
+    hoverBackground,
+    defaultBackground,
+}: {
+    canvasElement: HTMLElement;
+    defaultBackground: string;
+    hoverBackground: string;
+}) => {
+    const canvas = within(canvasElement);
+
+    await expect(
+        window.getComputedStyle(canvas.getByRole('button').parentElement as HTMLElement)
+            .backgroundColor,
+    ).toBe(defaultBackground);
+
+    await delay(500);
+
+    await userEvent.hover(canvas.getByRole('button'));
+
+    await delay(500);
+
+    await expect(
+        window.getComputedStyle(canvas.getByRole('button').parentElement as HTMLElement)
+            .backgroundColor,
+    ).toBe(hoverBackground);
+
+    await delay(500);
+
+    await userEvent.unhover(canvas.getByRole('button'));
+
+    await delay(500);
+
+    await expect(
+        window.getComputedStyle(canvas.getByRole('button').parentElement as HTMLElement)
+            .backgroundColor,
+    ).toBe(defaultBackground);
+};
+
+Contained.play = ({ canvasElement }) =>
+    hoverInteractions({
+        canvasElement,
+        defaultBackground: 'rgb(103, 80, 164)',
+        hoverBackground: 'rgb(98, 76, 156)',
+    });
+
+ContainedTonal.play = ({ canvasElement }) =>
+    hoverInteractions({
+        canvasElement,
+        defaultBackground: 'rgb(232, 222, 248)',
+        hoverBackground: 'rgb(201, 197, 209)',
+    });
+
+Elevated.play = ({ canvasElement }) =>
+    hoverInteractions({
+        canvasElement,
+        defaultBackground: 'rgba(103, 80, 164, 0.05)',
+        hoverBackground: 'rgb(241, 239, 247)',
+    });
+
+Outlined.play = ({ canvasElement }) =>
+    hoverInteractions({
+        canvasElement,
+        defaultBackground: 'rgba(0, 0, 0, 0)',
+        hoverBackground: 'rgb(241, 239, 247)',
+    });
+
+Text.play = ({ canvasElement }) =>
+    hoverInteractions({
+        canvasElement,
+        defaultBackground: 'rgba(0, 0, 0, 0)',
+        hoverBackground: 'rgb(241, 239, 247)',
+    });
