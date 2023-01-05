07a6cef7c8ea0055ce5dbd7e2172f416f2ceaa90
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2023-01-05 10:55:28 +0300

    replaced iconbuttons with Appbar.Actions

--- example/stories/components/Appbar/Appbar.tsx
+++ example/stories/components/Appbar/Appbar.tsx
@@ -4,93 +4,93 @@ import { useMolecules, AppbarProps } from 'bamboo-molecules';
 export type Props = Omit<AppbarProps, 'children'>;
 
 export const Example = ({ style, ...rest }: Props) => {
-    const { Appbar, IconButton } = useMolecules();
+    const { Appbar } = useMolecules();
     const { width } = useWindowDimensions();
 
     return (
         <Appbar style={StyleSheet.flatten([{ width }, style]) as ViewStyle} {...rest}>
             <Appbar.Left>
-                <IconButton name="menu" onPress={() => {}} />
+                <Appbar.Actions name="menu" onPress={() => {}} />
             </Appbar.Left>
             <Appbar.Title>Default Appbar</Appbar.Title>
             <Appbar.Right>
-                <IconButton name="star-outline" onPress={() => {}} />
-                <IconButton name="pin-outline" onPress={() => {}} />
-                <IconButton name="dots-vertical" onPress={() => {}} />
+                <Appbar.Actions name="star-outline" onPress={() => {}} />
+                <Appbar.Actions name="pin-outline" onPress={() => {}} />
+                <Appbar.Actions name="dots-vertical" onPress={() => {}} />
             </Appbar.Right>
         </Appbar>
     );
 };
 export const ExampleCenterAligned = ({ style, ...rest }: Props) => {
-    const { Appbar, IconButton } = useMolecules();
+    const { Appbar } = useMolecules();
     const { width } = useWindowDimensions();
 
     return (
         <Appbar.CenterAligned style={StyleSheet.flatten([{ width }, style]) as ViewStyle} {...rest}>
             <Appbar.Left>
-                <IconButton name="menu" onPress={() => {}} />
+                <Appbar.Actions name="menu" onPress={() => {}} />
             </Appbar.Left>
             <Appbar.Title>Center-Aligned Appbar</Appbar.Title>
             <Appbar.Right>
-                <IconButton name="heart-outline" onPress={() => {}} />
-                <IconButton name="dots-vertical" onPress={() => {}} />
+                <Appbar.Actions name="heart-outline" onPress={() => {}} />
+                <Appbar.Actions name="dots-vertical" onPress={() => {}} />
             </Appbar.Right>
         </Appbar.CenterAligned>
     );
 };
 
 export const ExampleSmall = ({ style, ...rest }: Props) => {
-    const { Appbar, IconButton } = useMolecules();
+    const { Appbar } = useMolecules();
     const { width } = useWindowDimensions();
 
     return (
         <Appbar.Small style={StyleSheet.flatten([{ width }, style]) as ViewStyle} {...rest}>
             <Appbar.Left>
-                <IconButton name="arrow-left" onPress={() => {}} />
+                <Appbar.Actions name="arrow-left" onPress={() => {}} />
             </Appbar.Left>
             <Appbar.Title>Small Appbar</Appbar.Title>
             <Appbar.Right>
-                <IconButton name="phone-outline" onPress={() => {}} />
-                <IconButton name="magnify" onPress={() => {}} />
-                <IconButton name="dots-vertical" onPress={() => {}} />
+                <Appbar.Actions name="phone-outline" onPress={() => {}} />
+                <Appbar.Actions name="magnify" onPress={() => {}} />
+                <Appbar.Actions name="dots-vertical" onPress={() => {}} />
             </Appbar.Right>
         </Appbar.Small>
     );
 };
 
 export const ExampleMedium = ({ style, ...rest }: Props) => {
-    const { Appbar, IconButton } = useMolecules();
+    const { Appbar } = useMolecules();
     const { width } = useWindowDimensions();
 
     return (
         <Appbar.Medium style={StyleSheet.flatten([{ width }, style]) as ViewStyle} {...rest}>
             <Appbar.Left>
-                <IconButton name="arrow-left" onPress={() => {}} />
+                <Appbar.Actions name="arrow-left" onPress={() => {}} />
             </Appbar.Left>
             <Appbar.Title>Medium Appbar</Appbar.Title>
             <Appbar.Right>
-                <IconButton name="paperclip" onPress={() => {}} />
-                <IconButton name="calendar" onPress={() => {}} />
-                <IconButton name="dots-vertical" onPress={() => {}} />
+                <Appbar.Actions name="paperclip" onPress={() => {}} />
+                <Appbar.Actions name="calendar" onPress={() => {}} />
+                <Appbar.Actions name="dots-vertical" onPress={() => {}} />
             </Appbar.Right>
         </Appbar.Medium>
     );
 };
 
 export const ExampleLarge = ({ style = {}, ...rest }: Props) => {
-    const { Appbar, IconButton } = useMolecules();
+    const { Appbar } = useMolecules();
     const { width } = useWindowDimensions();
 
     return (
         <Appbar.Large style={StyleSheet.flatten([{ width }, style]) as ViewStyle} {...rest}>
             <Appbar.Left>
-                <IconButton name="arrow-left" onPress={() => {}} />
+                <Appbar.Actions name="arrow-left" onPress={() => {}} />
             </Appbar.Left>
             <Appbar.Title>Large Appbar</Appbar.Title>
             <Appbar.Right>
-                <IconButton name="paperclip" onPress={() => {}} />
-                <IconButton name="calendar" onPress={() => {}} />
-                <IconButton name="dots-vertical" onPress={() => {}} />
+                <Appbar.Actions name="paperclip" onPress={() => {}} />
+                <Appbar.Actions name="calendar" onPress={() => {}} />
+                <Appbar.Actions name="dots-vertical" onPress={() => {}} />
             </Appbar.Right>
         </Appbar.Large>
     );
