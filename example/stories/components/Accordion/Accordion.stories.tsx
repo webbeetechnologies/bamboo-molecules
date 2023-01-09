77eb5f9db1a36bd110d1a00a252fb3946c7f0897
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2023-01-09 10:57:21 +0300

    added more stories

--- example/stories/components/Accordion/Accordion.stories.tsx
+++ example/stories/components/Accordion/Accordion.stories.tsx
@@ -1,6 +1,6 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { Example } from './Accordion';
+import { Example, ExampleControlled, ExampleNestedAccordion } from './Accordion';
 
 export default {
     title: 'components/Accordion',
@@ -9,12 +9,185 @@ export default {
 
 export const Default: ComponentStory<typeof Example> = args => <Example {...args} />;
 
-Default.args = {};
+Default.args = {
+    multiple: true,
+};
 
 Default.parameters = {
     docs: {
         source: {
-            code: ``,
+            code: `
+    const { Accordion, AccordionItem, Icon, View, Text } = useMolecules();
+    const leftElement = useCallback(
+        ({ expanded, color }: AccordionHeaderElementProps) => (
+            <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
+        ),
+        [Icon],
+    );
+
+    return (
+        <Accordion multiple>
+            <AccordionItem style={styles.item} id="1">
+                <AccordionItem.Header left={leftElement}>First Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+            <AccordionItem style={styles.item} id="2">
+                <AccordionItem.Header left={leftElement}>Second Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+        </Accordion>
+    );
+            `,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
+
+export const Controlled: ComponentStory<typeof ExampleControlled> = args => (
+    <ExampleControlled {...args} />
+);
+
+Controlled.args = {};
+
+Controlled.parameters = {
+    controls: {
+        exclude: ['multiple', 'expandedItems', 'onChange'],
+    },
+    docs: {
+        source: {
+            code: `
+    const [expandedItems, setExpandedItems] = useState<string[]>([]);
+    const leftElement = useCallback(
+        ({ expanded, color }: AccordionHeaderElementProps) => (
+            <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
+        ),
+        [Icon],
+    );
+
+    const onExpandedItemsChange = useCallback(
+        (ids: string | string[]) => setExpandedItems(ids as string[]),
+        [],
+    );
+
+    return (
+        <Accordion multiple expandedItemIds={expandedItems} onChange={onExpandedItemsChange}>
+            <AccordionItem style={styles.item} id="1">
+                <AccordionItem.Header left={leftElement}>First Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+            <AccordionItem style={styles.item} id="2">
+                <AccordionItem.Header left={leftElement}>Second Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+        </Accordion>
+    );
+            `,
+            language: 'tsx',
+            type: 'auto',
+        },
+    },
+};
+
+export const NestedAccordion: ComponentStory<typeof ExampleNestedAccordion> = args => (
+    <ExampleNestedAccordion {...args} />
+);
+
+NestedAccordion.args = {
+    multiple: true,
+};
+
+NestedAccordion.parameters = {
+    docs: {
+        source: {
+            code: `
+            const { Accordion, AccordionItem, Icon, View, Text } = useMolecules();
+    const leftElement = useCallback(
+        ({ expanded, color }: AccordionHeaderElementProps) => (
+            <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
+        ),
+        [Icon],
+    );
+
+    return (
+        <Accordion multiple>
+            <AccordionItem style={styles.item} id="1">
+                <AccordionItem.Header left={leftElement}>
+                    Accordion (Accordion Group)
+                </AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+            <AccordionItem style={styles.item} id="2">
+                <AccordionItem.Header left={leftElement}>AccordionItem</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <Accordion multiple>
+                        <AccordionItem style={styles.item} id="1">
+                            <AccordionItem.Header left={leftElement}>
+                                Accordion Header
+                            </AccordionItem.Header>
+                            <AccordionItem.Content>
+                                <View style={styles.content}>
+                                    <Text style={styles.text}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Atque temporibus ut vero. Ducimus
+                                    </Text>
+                                </View>
+                            </AccordionItem.Content>
+                        </AccordionItem>
+                        <AccordionItem style={styles.item} id="2">
+                            <AccordionItem.Header left={leftElement}>
+                                Accordion Content
+                            </AccordionItem.Header>
+                            <AccordionItem.Content>
+                                <View style={styles.content}>
+                                    <Text style={styles.text}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Atque temporibus ut vero. Ducimus
+                                    </Text>
+                                </View>
+                            </AccordionItem.Content>
+                        </AccordionItem>
+                    </Accordion>
+                </AccordionItem.Content>
+            </AccordionItem>
+        </Accordion>
+    );
+            `,
             language: 'tsx',
             type: 'auto',
         },
--- example/stories/components/Accordion/Accordion.tsx
+++ example/stories/components/Accordion/Accordion.tsx
@@ -1,12 +1,12 @@
-import { useMolecules, AccordionItemProps } from 'bamboo-molecules';
-import { useCallback } from 'react';
+import { useMolecules, AccordionProps, AccordionHeaderElementProps } from 'bamboo-molecules';
+import { useCallback, useState } from 'react';
 
-export type Props = AccordionItemProps & {};
+export type Props = AccordionProps & {};
 
 export const Example = (props: Props) => {
     const { Accordion, AccordionItem, Icon, View, Text } = useMolecules();
     const leftElement = useCallback(
-        ({ expanded, color }: any) => (
+        ({ expanded, color }: AccordionHeaderElementProps) => (
             <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
         ),
         [Icon],
@@ -34,6 +34,114 @@ export const Example = (props: Props) => {
                             temporibus ut vero. Ducimus
                         </Text>
                     </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+        </Accordion>
+    );
+};
+
+export const ExampleControlled = (props: Props) => {
+    const { Accordion, AccordionItem, Icon, View, Text } = useMolecules();
+    const [expandedItems, setExpandedItems] = useState<string[]>([]);
+    const leftElement = useCallback(
+        ({ expanded, color }: AccordionHeaderElementProps) => (
+            <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
+        ),
+        [Icon],
+    );
+
+    const onExpandedItemsChange = useCallback(
+        (ids: string | string[]) => setExpandedItems(ids as string[]),
+        [],
+    );
+
+    return (
+        <Accordion
+            {...props}
+            multiple
+            expandedItemIds={expandedItems}
+            onChange={onExpandedItemsChange}>
+            <AccordionItem style={{ width: 400 }} id="1">
+                <AccordionItem.Header left={leftElement}>First Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={{ padding: 'spacings.4' }}>
+                        <Text style={{ fontSize: 16 }}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+            <AccordionItem style={{ width: 400 }} id="2">
+                <AccordionItem.Header left={leftElement}>Second Item</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={{ padding: 'spacings.4' }}>
+                        <Text style={{ fontSize: 16 }}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+        </Accordion>
+    );
+};
+
+export const ExampleNestedAccordion = (props: Props) => {
+    const { Accordion, AccordionItem, Icon, View, Text } = useMolecules();
+    const leftElement = useCallback(
+        ({ expanded, color }: AccordionHeaderElementProps) => (
+            <Icon color={color} name={expanded ? 'chevron-up' : 'chevron-down'} size={24} />
+        ),
+        [Icon],
+    );
+
+    return (
+        <Accordion {...props}>
+            <AccordionItem style={{ width: 400 }} id="1">
+                <AccordionItem.Header left={leftElement}>
+                    Accordion (Accordion Group)
+                </AccordionItem.Header>
+                <AccordionItem.Content>
+                    <View style={{ padding: 'spacings.4' }}>
+                        <Text style={{ fontSize: 16 }}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+                            temporibus ut vero. Ducimus
+                        </Text>
+                    </View>
+                </AccordionItem.Content>
+            </AccordionItem>
+            <AccordionItem style={{ width: 400 }} id="2">
+                <AccordionItem.Header left={leftElement}>AccordionItem</AccordionItem.Header>
+                <AccordionItem.Content>
+                    <Accordion multiple>
+                        <AccordionItem style={{ width: 400 }} id="1">
+                            <AccordionItem.Header left={leftElement}>
+                                Accordion Header
+                            </AccordionItem.Header>
+                            <AccordionItem.Content>
+                                <View style={{ padding: 'spacings.4' }}>
+                                    <Text style={{ fontSize: 16 }}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Atque temporibus ut vero. Ducimus
+                                    </Text>
+                                </View>
+                            </AccordionItem.Content>
+                        </AccordionItem>
+                        <AccordionItem style={{ width: 400 }} id="2">
+                            <AccordionItem.Header left={leftElement}>
+                                Accordion Content
+                            </AccordionItem.Header>
+                            <AccordionItem.Content>
+                                <View style={{ padding: 'spacings.4' }}>
+                                    <Text style={{ fontSize: 16 }}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Atque temporibus ut vero. Ducimus
+                                    </Text>
+                                </View>
+                            </AccordionItem.Content>
+                        </AccordionItem>
+                    </Accordion>
                 </AccordionItem.Content>
             </AccordionItem>
         </Accordion>
