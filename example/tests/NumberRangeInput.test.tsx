ef221a684d5be7c90f025a90d49b22195ca6fb85
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2023-01-09 11:51:51 +0300

    updated the tests

--- example/tests/NumberRangeInput.test.tsx
+++ example/tests/NumberRangeInput.test.tsx
@@ -2,16 +2,19 @@ import { renderWithWrapper } from '../testHelpers';
 import { NumberRangeInput } from '../../src/components/NumberRangeInput';
 
 const mockedOnChange = jest.fn();
+const mockedValue = { min: '10', max: '15' };
 
 describe('NumberRangeInput', () => {
     it('should render correctly with default props', () => {
-        const tree = renderWithWrapper(<NumberRangeInput />).toJSON();
+        const tree = renderWithWrapper(
+            <NumberRangeInput value={mockedValue} onChange={mockedOnChange} />,
+        ).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
     it('should correctly mask the defaultValue', () => {
         const { getByDisplayValue } = renderWithWrapper(
-            <NumberRangeInput defaultValue={{ min: '10', max: '15' }} />,
+            <NumberRangeInput value={mockedValue} onChange={mockedOnChange} />,
         );
         expect(getByDisplayValue('10')).toBeTruthy();
         expect(getByDisplayValue('15')).toBeTruthy();
