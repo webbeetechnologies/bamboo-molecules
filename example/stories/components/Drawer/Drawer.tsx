3b5072f6029a843dbb1d87929f5b40c7f07b3afd
Thet Aung <maksimsenkevich@MacBook-Pro-von-Maksim.local>
2023-01-11 08:20:53 +0300

    updated the story with DrawerItemGroup

--- example/stories/components/Drawer/Drawer.tsx
+++ example/stories/components/Drawer/Drawer.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
+import { Text, useWindowDimensions } from 'react-native';
 import { useMolecules, DrawerProps, DrawerItemProps } from 'bamboo-molecules';
-import { StyleSheet, Text, TextStyle, useWindowDimensions } from 'react-native';
 
 export type Props = DrawerProps & DrawerItemProps['left'] & {};
 
 export const Example = (props: Props) => {
-    const { Drawer, Icon, HorizontalDivider } = useMolecules();
+    const { Drawer, Icon } = useMolecules();
     const { height } = useWindowDimensions();
 
     const [activeItem, setActiveItem] = useState(0);
@@ -16,61 +16,62 @@ export const Example = (props: Props) => {
                 <Text>Header</Text>
             </Drawer.Header>
             <Drawer.Content>
-                <Text style={styles.sectionHeadline}>Mail</Text>
-                <Drawer.Item
-                    label="Inbox"
-                    left={({ color }) => <Icon name="inbox" color={color} size={24} />}
-                    active={activeItem === 0}
-                    onPress={() => setActiveItem(0)}
-                    right={({ color }) => <Text style={{ color }}>24</Text>}
-                />
+                <Drawer.ItemGroup title="Mail" showDivider>
+                    <Drawer.Item
+                        label="Inbox"
+                        left={({ color }) => <Icon name="inbox" color={color} size={24} />}
+                        active={activeItem === 0}
+                        onPress={() => setActiveItem(0)}
+                        right={({ color }) => <Text style={{ color }}>24</Text>}
+                    />
 
-                <Drawer.Item
-                    label="Outbox"
-                    left={({ color }) => <Icon name="send-outline" color={color} size={24} />}
-                    active={activeItem === 1}
-                    onPress={() => setActiveItem(1)}
-                    right={({ color }) => <Text style={{ color }}>100+</Text>}
-                />
-                <Drawer.Item
-                    label="Favorites"
-                    left={({ color }) => <Icon name="heart-outline" color={color} size={24} />}
-                    active={activeItem === 2}
-                    onPress={() => setActiveItem(2)}
-                />
-                <Drawer.Item
-                    label="Trash"
-                    left={({ color }) => <Icon name="delete-outline" color={color} size={24} />}
-                    active={activeItem === 3}
-                    onPress={() => setActiveItem(3)}
-                />
-                <HorizontalDivider spacing={4} />
-                <Text style={styles.sectionHeadline}>Labels</Text>
-                <Drawer.Item
-                    label="Inbox"
-                    left={({ color }) => <Icon name="inbox" color={color} size={24} />}
-                    active={activeItem === 4}
-                    onPress={() => setActiveItem(4)}
-                />
+                    <Drawer.Item
+                        label="Outbox"
+                        left={({ color }) => <Icon name="send-outline" color={color} size={24} />}
+                        active={activeItem === 1}
+                        onPress={() => setActiveItem(1)}
+                        right={({ color }) => <Text style={{ color }}>100+</Text>}
+                    />
+                    <Drawer.Item
+                        label="Favorites"
+                        left={({ color }) => <Icon name="heart-outline" color={color} size={24} />}
+                        active={activeItem === 2}
+                        onPress={() => setActiveItem(2)}
+                    />
+                    <Drawer.Item
+                        label="Trash"
+                        left={({ color }) => <Icon name="delete-outline" color={color} size={24} />}
+                        active={activeItem === 3}
+                        onPress={() => setActiveItem(3)}
+                    />
+                </Drawer.ItemGroup>
+                <Drawer.ItemGroup title="Labels" showDivider>
+                    <Drawer.Item
+                        label="Inbox"
+                        left={({ color }) => <Icon name="inbox" color={color} size={24} />}
+                        active={activeItem === 4}
+                        onPress={() => setActiveItem(4)}
+                    />
 
-                <Drawer.Item
-                    label="Outbox"
-                    left={({ color }) => <Icon name="send-outline" color={color} size={24} />}
-                    active={activeItem === 5}
-                    onPress={() => setActiveItem(5)}
-                />
-                <Drawer.Item
-                    label="Favorites"
-                    left={({ color }) => <Icon name="heart-outline" color={color} size={24} />}
-                    active={activeItem === 6}
-                    onPress={() => setActiveItem(6)}
-                />
-                <Drawer.Item
-                    label="Trash"
-                    left={({ color }) => <Icon name="delete-outline" color={color} size={24} />}
-                    active={activeItem === 7}
-                    onPress={() => setActiveItem(7)}
-                />
+                    <Drawer.Item
+                        label="Outbox"
+                        left={({ color }) => <Icon name="send-outline" color={color} size={24} />}
+                        active={activeItem === 5}
+                        onPress={() => setActiveItem(5)}
+                    />
+                    <Drawer.Item
+                        label="Favorites"
+                        left={({ color }) => <Icon name="heart-outline" color={color} size={24} />}
+                        active={activeItem === 6}
+                        onPress={() => setActiveItem(6)}
+                    />
+                    <Drawer.Item
+                        label="Trash"
+                        left={({ color }) => <Icon name="delete-outline" color={color} size={24} />}
+                        active={activeItem === 7}
+                        onPress={() => setActiveItem(7)}
+                    />
+                </Drawer.ItemGroup>
             </Drawer.Content>
             <Drawer.Footer style={{ height: 50, padding: 12 }}>
                 <Text>Footer</Text>
@@ -78,14 +79,3 @@ export const Example = (props: Props) => {
         </Drawer>
     );
 };
-
-const styles = StyleSheet.create({
-    sectionHeadline: {
-        color: 'colors.onSurfaceVariant',
-        lineHeight: 'typescale.titleSmall.lineHeight' as unknown as number,
-        fontSize: 'typescale.titleSmall.fontSize' as unknown as number,
-        fontWeight: 'typescale.titleSmall.fontWeight' as unknown as TextStyle['fontWeight'],
-        marginVertical: 16,
-        marginHorizontal: 12,
-    },
-});
