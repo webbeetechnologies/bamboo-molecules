bd9b19dae1373608b7ff90f7cf526d4e2f67d4f2
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-01-19 18:14:26 +0300

    cleanup the interaction tests

--- example/stories/components/Rating/Rating.stories.tsx
+++ example/stories/components/Rating/Rating.stories.tsx
@@ -35,92 +35,61 @@ Default.play = async ({ canvasElement }) => {
     const activeColor = 'rgb(250, 175, 0)';
 
     await waitFor(() => canvas);
+    await delay(500);
 
-    await delay(100);
+    const ratingItem1 = await canvas.getByTestId('ratingInteraction-rating-item-1');
+    const ratingItem2 = await canvas.getByTestId('ratingInteraction-rating-item-2');
+    const ratingItem3 = await canvas.getByTestId('ratingInteraction-rating-item-3');
+    const ratingItem4 = await canvas.getByTestId('ratingInteraction-rating-item-4');
+    const ratingItem5 = await canvas.getByTestId('ratingInteraction-rating-item-5');
 
     // initially everything should have default color
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-1')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem1).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-2')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem2).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-3')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem3).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-4')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem4).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-5')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem5).color).toBe(defaultColor);
 
     // after clicking to icon 3, icon 1, 2, and 3 should be active
-    await userEvent.click(canvas.getByTestId('ratingInteraction-rating-item-3'));
+    await userEvent.click(ratingItem3);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-1')).color,
-    ).toBe(activeColor);
+    await expect(window.getComputedStyle(ratingItem1).color).toBe(activeColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-2')).color,
-    ).toBe(activeColor);
+    await expect(window.getComputedStyle(ratingItem2).color).toBe(activeColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-3')).color,
-    ).toBe(activeColor);
+    await expect(window.getComputedStyle(ratingItem3).color).toBe(activeColor);
 
     // the rest should still be default color
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-4')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem4).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-5')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem5).color).toBe(defaultColor);
 
     // after clicking to icon number 2, only icon 1 and 2 should be active and 3 should be inactive
-    await userEvent.click(canvas.getByTestId('ratingInteraction-rating-item-2'));
+    await userEvent.click(ratingItem2);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-1')).color,
-    ).toBe(activeColor);
+    await expect(window.getComputedStyle(ratingItem1).color).toBe(activeColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-2')).color,
-    ).toBe(activeColor);
+    await expect(window.getComputedStyle(ratingItem2).color).toBe(activeColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-3')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem3).color).toBe(defaultColor);
 
     // after clicking 2 again, everything should be inactive
-    await userEvent.click(canvas.getByTestId('ratingInteraction-rating-item-2'));
+    await userEvent.click(ratingItem2);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-1')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem1).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-2')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem2).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-3')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem3).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-4')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem4).color).toBe(defaultColor);
 
-    await expect(
-        window.getComputedStyle(canvas.getByTestId('ratingInteraction-rating-item-5')).color,
-    ).toBe(defaultColor);
+    await expect(window.getComputedStyle(ratingItem5).color).toBe(defaultColor);
 
-    await userEvent.unhover(canvas.getByTestId('ratingInteraction-rating-item-3'));
-    await userEvent.unhover(canvas.getByTestId('ratingInteraction-rating-item-2'));
+    await userEvent.unhover(ratingItem3);
+    await userEvent.unhover(ratingItem2);
 };
