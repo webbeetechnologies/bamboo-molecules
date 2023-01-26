2892a5cc33f8723d5aca633e0381e32cbe803024
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-01-26 09:38:11 +0300

    added interaction tests

--- example/stories/components/DateTimePicker/DateTimePicker.stories.tsx
+++ example/stories/components/DateTimePicker/DateTimePicker.stories.tsx
@@ -1,6 +1,9 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { Example } from './DateTimePicker';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { delay } from '../../common';
 
 export default {
     title: 'components/DateTimePicker',
@@ -9,7 +12,9 @@ export default {
 
 export const Default: ComponentStory<typeof Example> = args => <Example {...args} />;
 
-Default.args = {};
+Default.args = {
+    testID: 'datetimepicker',
+};
 
 Default.parameters = {
     docs: {
@@ -24,4 +29,23 @@ Default.parameters = {
             type: 'auto',
         },
     },
+};
+
+Default.play = async ({ canvasElement }) => {
+    const canvas = await within(canvasElement);
+
+    await waitFor(() => canvas);
+
+    const dateInput = await canvas.getByTestId('datetimepicker--datepickerinput-outlined');
+    const timeInput = await canvas.getByTestId('datetimepicker--timepickerinput-outlined');
+    // const timepickerModal = await canvas.getByTestId('datetimepicker--timepickermodal');
+
+    await userEvent.type(dateInput, '13/01/2022');
+
+    expect(canvas.getByDisplayValue('13/01/2022')).toBeInTheDocument();
+
+    await userEvent.type(timeInput, '1');
+    await userEvent.click(canvasElement);
+
+    expect(canvas.getByDisplayValue('01:00am')).toBeInTheDocument();
 };
