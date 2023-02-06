c165b74a16afc9f9c832f2a5e68fd7bc0f5b2b3a
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-02-06 07:00:28 +0300

    moved stories into the new folder

--- stories/components/DateTimePicker/DateTimePicker.tsx
+++ stories/components/DateTimePicker/DateTimePicker.tsx
@@ -1,10 +1,9 @@
-import { useMolecules, DateTimePickerProps } from 'bamboo-molecules';
+import { DateTimePicker, DateTimePickerProps } from '../../../src/components';
 import { useCallback, useState } from 'react';
 
 export type Props = DateTimePickerProps & {};
 
 export const Example = (props: Props) => {
-    const { DateTimePicker } = useMolecules();
     const [date, setDate] = useState<Date | null>(null);
 
     const onChange = useCallback(
--- stories/components/TimePickerField/TimePickerField.tsx
+++ stories/components/TimePickerField/TimePickerField.tsx
@@ -1,10 +1,9 @@
-import { useMolecules, TimePickerFieldProps } from 'bamboo-molecules';
 import { useCallback, useState } from 'react';
+import { TimePickerField, TimePickerFieldProps } from '../../../src/components';
 
 export type Props = TimePickerFieldProps & {};
 
 export const Example = (props: Props) => {
-    const { TimePickerField } = useMolecules();
     const [time, setTime] = useState<string>('10:13');
 
     const onChange = useCallback(
