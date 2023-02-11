2a305e727ae7164856b36101fc67d00fe641c3b1
Elvis Duru <duruelvis95@gmail.com>
2023-02-11 11:53:19 +0100

    feat: numberOfCols and referenceBreakpoints config

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,16 +1,45 @@
 import { FC, forwardRef, memo, useCallback, useMemo, useRef } from 'react';
 import { FlatList, FlatListProps, useWindowDimensions } from 'react-native';
 import { useComponentStyles } from '../../hooks';
-import type { ColumnProps } from '../Column';
-import Column from '../Column/Column';
+import type { ColumnProps } from './Column';
+import Column from './Column/Column';
 
-export type Props = FlatListProps<ColumnProps> & {
-    renderer: FC<{ name: string }>;
+const defaultBreakpoints = {
+    xs: 320,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
 };
 
-const keyExtractor = (item: ColumnProps) => item.name;
+const defaultNumberOfColumns = 12;
 
-function Grid({ data, style, renderer }: Props, ref: any) {
+type IColumnProps = Omit<ColumnProps, 'referenceBreakpoints' | 'numberOfColumns'>;
+
+export type Props = FlatListProps<IColumnProps> & {
+    renderer: FC<{ name: string }>;
+    referenceBreakpoints?: {
+        xs: number;
+        sm: number;
+        md: number;
+        lg: number;
+        xl: number;
+    };
+    numberOfColumns?: number;
+};
+
+const keyExtractor = (item: IColumnProps) => item.name;
+
+const Grid = (
+    {
+        data,
+        style,
+        renderer,
+        referenceBreakpoints = defaultBreakpoints,
+        numberOfColumns = defaultNumberOfColumns,
+    }: Props,
+    ref: any,
+) => {
     const { width } = useWindowDimensions();
     const componentStyles = useComponentStyles('Grid', style);
 
@@ -25,8 +54,11 @@ function Grid({ data, style, renderer }: Props, ref: any) {
     const MemoizedRenderer = useRef(renderer).current;
 
     const renderItem = useCallback(
-        ({ item }: { item: ColumnProps }) => (
-            <Column {...item}>
+        ({ item }: { item: IColumnProps }) => (
+            <Column
+                {...item}
+                referenceBreakpoints={referenceBreakpoints}
+                numberOfColumns={numberOfColumns}>
                 <MemoizedRenderer name={item.name} />
             </Column>
         ),
@@ -39,10 +71,10 @@ function Grid({ data, style, renderer }: Props, ref: any) {
             keyExtractor={keyExtractor}
             data={data}
             renderItem={renderItem}
-            numColumns={12}
+            numColumns={numberOfColumns}
             ref={ref}
         />
     );
-}
+};
 
 export default memo(forwardRef(Grid));
