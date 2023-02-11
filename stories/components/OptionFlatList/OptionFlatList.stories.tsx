db243c86420b90aab13b4bf9ceb93a5d5f7f8907
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-02-11 17:58:22 +0300

    updated the stories and docs

--- stories/components/OptionFlatList/OptionFlatList.stories.tsx
+++ stories/components/OptionFlatList/OptionFlatList.stories.tsx
@@ -4,21 +4,40 @@ import { generateFlatListData } from '../../common';
 import { Example as ListItem, ListItemTitle } from '../ListItem/ListItem';
 
 import { Example } from './OptionFlatList';
+import { useArgs } from '@storybook/addons';
+import { useCallback, useMemo } from 'react';
 
 export default {
     title: 'components/OptionFlatList',
     component: Example,
 } as ComponentMeta<typeof Example>;
 
-export const Default: ComponentStory<typeof Example> = args => (
-    <View style={{ minWidth: 300, maxHeight: 500 }}>
-        <Example {...args} />
-    </View>
-);
+export const Default: ComponentStory<typeof Example> = args => {
+    const [_, updateArgs] = useArgs();
+
+    const records = useMemo(() => {
+        return args.records.filter(item =>
+            item.title.toLowerCase().includes(args.query?.toLowerCase()),
+        );
+    }, [args.query, args.records]);
+
+    const onQueryChange = useCallback(
+        (text: string) => {
+            updateArgs({ ...args, query: text });
+        },
+        [args, updateArgs],
+    );
+
+    return (
+        <View style={{ minWidth: 300, maxHeight: 500 }}>
+            <Example {...args} records={records} onQueryChange={onQueryChange} />
+        </View>
+    );
+};
 
 Default.args = {
     searchable: true,
-    onQueryChange: () => {},
+    query: '',
     records: generateFlatListData(100),
     renderItem: ({ item }: any) => (
         <ListItem>
@@ -34,8 +53,45 @@ Default.parameters = {
     docs: {
         source: {
             code: `
-<OptionFlatList
-    records={[
+import { useState, useCallback, useMemo } from 'react';
+import { ListRenderItemInfo } from 'react-native';
+import { useMolecules } from '@bulterapp/bamboo-molecules';
+
+export const App = () => {
+    const { OptionFlatList, ListItem } = useMolecules();
+    const [query, setQuery] = useState();
+    
+    const records = useMemo(() => {
+        return defaultRecords.filter(item =>
+            item.title.toLowerCase().includes(query.toLowerCase()),
+        );
+    }, [query]);
+    
+    const onQueryChange = useCallback((text: string) => {
+      setQuery(text);
+    }, [setQuery]);
+    
+    const searchInputProps = useMemo(() => ({
+        label: 'Search . . .'
+    }), []);
+    
+    const renderItem = useCallback(({ item }: ListRenderItemInfo) => (
+        <ListItem>
+            <ListItem.Title>{item.title}</ListItem.Title>
+        </ListItem>
+    ), [])
+
+    return (
+        <OptionFlatList 
+            searchable query={query} 
+            onQueryChange={onQueryChange} 
+            records={records} 
+            renderItem={renderItem} 
+            searchInputProps={searchInputProps} />
+    );
+};
+
+const defaultRecords = [
           { title: "item 0" },
           { title: "item 1" },
           { title: "item 2" },
@@ -46,13 +102,8 @@ Default.parameters = {
           { title: "item 7" },
           { title: "item 8" },
           // . . .
-          ]}
-    renderItem={({ item }) => (
-        <ListItem>
-            <ListItem.Title>{item}</ListItem.Title>
-        </ListItem>
-    )}
-  />`,
+          ]
+`,
             language: 'tsx',
             type: 'auto',
         },
--- stories/components/OptionList/OptionList.tsx
+++ stories/components/OptionList/OptionList.tsx
@@ -1,7 +1,37 @@
-import { OptionList, OptionListProps } from '../../../src/components';
+import { useMolecules } from '../../../src';
+import { OptionList, ListItem, OptionListProps } from '../../../src/components';
+import { useCallback } from 'react';
+import { StyleSheet } from 'react-native';
 
 export type Props = OptionListProps & {};
 
 export const Example = (props: Props) => {
-    return <OptionList {...props} />;
+    const { Text } = useMolecules();
+
+    const renderItem = useCallback(
+        ({ item }: any) => (
+            <ListItem>
+                <ListItem.Title>{item.title}</ListItem.Title>
+            </ListItem>
+        ),
+        [],
+    );
+    const renderSectionHeader = useCallback(
+        ({ section }: any) => <Text style={styles.sectionTitle}>{section.title}</Text>,
+        [Text],
+    );
+
+    return (
+        <OptionList renderItem={renderItem} renderSectionHeader={renderSectionHeader} {...props} />
+    );
 };
+
+const styles = StyleSheet.create({
+    sectionTitle: {
+        fontSize: 15,
+        fontWeight: '600',
+        paddingLeft: 'spacings.3',
+        backgroundColor: 'colors.surface',
+        paddingTop: 'spacings.2',
+    },
+});
