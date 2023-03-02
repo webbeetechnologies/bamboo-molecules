e4d70036d18c4aea7acae97f37da9d740e212178
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-03-02 12:25:12 +0300

    chore: exported renderItem types from Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,2 +1,9 @@
-export { default as Select, Props as SelectProps, ISelect, SelectHandles } from './Select'; // to import in ComponentsProvider
+export {
+    default as Select,
+    Props as SelectProps,
+    ISelect,
+    SelectHandles,
+    SelectRenderItem,
+    SelectRenderItemInfo,
+} from './Select'; // to import in ComponentsProvider
 export { selectStyles } from './utils'; // to import in ThemeProvider
