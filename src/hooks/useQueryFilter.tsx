c53f92eb0f951aab9429be5418030a86fadb5dc7
Thet Aung <maksimsenkevich@MacBook-Pro-Thet.local>
2023-03-08 11:25:15 +0300

    feat: created useQueryFilter hook

---
+++ src/hooks/useQueryFilter.tsx
@@ -0,0 +1,29 @@
+import { useMemo, useState } from 'react';
+
+type DefaultItem = { id: string | number; [key: string]: any };
+
+export type UseQueryFilterProps<TItem extends DefaultItem = DefaultItem> = {
+    records: TItem[];
+    filterKey?: string;
+};
+
+type ReturnType<TItem extends DefaultItem = DefaultItem> = [
+    records: TItem[],
+    query: string,
+    onChange: (value: string) => void,
+];
+
+export const useQueryFilter = <TItem extends DefaultItem = DefaultItem>({
+    records: _records,
+    filterKey = 'label',
+}: UseQueryFilterProps<TItem>): ReturnType<TItem> => {
+    const [query, setQuery] = useState('');
+
+    const records = useMemo(
+        // TODO: match all unicode character with regex
+        () => _records.filter(item => item[filterKey].toLowerCase().includes(query.toLowerCase())),
+        [_records, filterKey, query],
+    );
+
+    return useMemo(() => [records, query, setQuery], [records, query, setQuery]);
+};
