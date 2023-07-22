89235e2e9c61338ddb2249f7c919a12291c7dec7
Thet Aung <thetaung.dev@gmail.com>
2023-07-22 17:55:31 +0300

    fix: ts issues

--- src/datagrid/field-types/Number/index.tsx
+++ src/datagrid/field-types/Number/index.tsx
@@ -1,9 +1,9 @@
 import type { FieldType } from '../../types';
 import EditorRenderer from './EditorRenderer';
 import ValueRenderer from './ValueRenderer';
-import type { Value, Config } from './types';
+import type { Value } from './types';
 
-export const NumberFieldType: FieldType<Value, Config> = {
+export const NumberFieldType: FieldType<Value> = {
     type: 'number',
     title: 'Number',
     icon: {
--- src/datagrid/field-types/SingleLineText/index.tsx
+++ src/datagrid/field-types/SingleLineText/index.tsx
@@ -1,9 +1,9 @@
 import type { FieldType } from '../../types';
 import EditorRenderer from './EditorRenderer';
 import ValueRenderer from './ValueRenderer';
-import type { Value, Config } from './types';
+import type { Value } from './types';
 
-export const SingleLineTextFieldType: FieldType<Value, Config> = {
+export const SingleLineTextFieldType: FieldType<Value> = {
     type: 'singleLineText',
     title: 'Single line text',
     icon: {
