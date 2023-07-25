562fde735d75b74823ffd6f03e1fbd173e754778
Thet Aung <thetaung.dev@gmail.com>
2023-07-25 12:10:15 +0300

    feat: exported createFieldRenderer factory function

--- src/datagrid/components/FieldRenderers/index.tsx
+++ src/datagrid/components/FieldRenderers/index.tsx
@@ -1,2 +1,3 @@
 export { default as EditRenderer } from './EditRenderer';
 export { default as ViewRenderer, ViewRendererProps } from './ViewRenderer';
+export { default as createFieldRenderer } from './createFieldRenderer';
