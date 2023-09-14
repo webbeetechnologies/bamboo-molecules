0a3d794f5c7ea97a26d59d180837afbed21a1a45
Thet Aung <thetaung.dev@gmail.com>
2023-09-14 13:37:04 +0300

    feat: added a feature that records the pressed keys, created useIsKeyPressed hoook

--- src/shortcuts-manager/index.tsx
+++ src/shortcuts-manager/index.tsx
@@ -1,4 +1,5 @@
 export * from './useSetScopes';
 export * from './ShortcutsManager';
 export * from './useShortcut';
+export * from './useIsKeyPressed';
 export * from './types';
---
+++ src/shortcuts-manager/useIsKeyPressed/index.tsx
@@ -0,0 +1,1 @@
+export { default as useIsKeyPress } from './useIsKeyPress';
