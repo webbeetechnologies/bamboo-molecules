5795633086960e85d1c3196ba2ff74b48462e148
Thet Aung <thetaung.dev@gmail.com>
2023-09-16 00:36:32 +0300

    improvement: removed unnecessary selector, used only the refs

--- src/shortcuts-manager/useShortcut/useShortcut.tsx
+++ src/shortcuts-manager/useShortcut/useShortcut.tsx
@@ -2,61 +2,50 @@ import { useCallback, useEffect } from 'react';
 import { Platform } from 'react-native';
 import { useLatest } from '@bambooapp/bamboo-molecules';
 
-import { useShortcutsManagerContextValueSelector } from '../ShortcutsManager';
-import type { Scope, ShortcutEventDetail } from '../types';
+import { useShortcutsManagerStoreRef } from '../ShortcutsManager';
+import type { ShortcutEventDetail } from '../types';
 import { calculateShortcutEventName } from '../utils';
 import type { ShortcutCallbackArgs } from './types';
 
 const useShortcut = (name: string, callback: (args: ShortcutCallbackArgs) => void) => {
-    const { shortcut, isActive, scope } = useShortcutsManagerContextValueSelector(store => {
-        const _shortcut = store.shortcuts[name];
+    const { store } = useShortcutsManagerStoreRef();
 
-        if (!_shortcut) {
-            throw new Error(`Shortcut with name: ${name} does not exist`);
-        }
-
-        return {
-            shortcut: _shortcut,
-            isActive: _shortcut.scope ? store.scopes[_shortcut.scope] : true,
-            scope: _shortcut.scope ? store.scopes[_shortcut.scope] : undefined,
-        };
-    });
-
-    const scopeRef = useLatest<Scope | undefined>(scope);
-    const activeRef = useLatest(isActive);
-    const shortcutRef = useLatest(shortcut);
+    const nameRef = useLatest(name);
     const callbackRef = useLatest(callback);
 
     const onCallback = useCallback(
         (e: CustomEvent<ShortcutEventDetail>) => {
-            if (!activeRef.current) return;
+            const shortcut = store.current.shortcuts[nameRef.current];
+            const scope = store.current.scopes[shortcut.scope || ''];
+
+            if (shortcut.scope && !scope) return;
 
             // if there is node, we check if one of the elements instead it is focused
             if (
-                scopeRef.current?.node &&
+                scope.node &&
                 !(
-                    scopeRef.current?.node === document.activeElement ||
-                    scopeRef.current?.node.contains(document.activeElement)
+                    scope.node === document.activeElement ||
+                    scope.node.contains(document.activeElement)
                 )
             )
                 return;
 
-            callbackRef.current({ ...e.detail, shortcut: shortcutRef.current });
+            callbackRef.current({ ...e.detail, shortcut: shortcut });
         },
-        [activeRef, callbackRef, scopeRef, shortcutRef],
+        [callbackRef, nameRef, store],
     );
 
     useEffect(() => {
         if (Platform.OS !== 'web') return;
 
-        const eventName = calculateShortcutEventName(shortcutRef.current.name);
+        const eventName = calculateShortcutEventName(nameRef.current);
 
         document.addEventListener(eventName, onCallback as (e: Event) => void);
 
         return () => {
             document.removeEventListener(eventName, onCallback as (e: Event) => void);
         };
-    }, [onCallback, scopeRef, shortcutRef]);
+    }, [onCallback, nameRef]);
 };
 
 export default useShortcut;
