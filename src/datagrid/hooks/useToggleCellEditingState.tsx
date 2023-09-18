eedb58acd5b317835cb043e7df132507cf3f06a3
Thet Aung <thetaung.dev@gmail.com>
2023-09-18 17:04:14 +0300

    chore: handle focusedCell undefined case

--- src/datagrid/hooks/useToggleCellEditingState.tsx
+++ src/datagrid/hooks/useToggleCellEditingState.tsx
@@ -10,9 +10,9 @@ const useToggleCellEditingState = () => {
     const { focusedCell } = usePluginsDataValueSelector(store => ({
         focusedCell: store[CELL_FOCUS_PLUGIN_KEY]?.focusedCell,
     }));
-    const { type } = useField(focusedCell?.columnId);
+    const { type } = useField(focusedCell?.columnId) || {};
 
-    const { toggleRenderersWithEnterKey } = useFieldType(type);
+    const { toggleRenderersWithEnterKey } = useFieldType(type) || {};
     const { useSetFocusCellPluginStore } = useCellFocusMethods();
     const setFocusCellPluginStore = useSetFocusCellPluginStore();
 
