67210b30cc015eb8c1518eb59945099843418e1f
elvisduru <duruelvis95@gmail.com>
2023-11-14 12:50:33 +0300

    Fix: frozen hover state in RowRenderer

--- src/components/DataTable/hoc/withRowLoadingPlaceholder.tsx
+++ src/components/DataTable/hoc/withRowLoadingPlaceholder.tsx
@@ -15,13 +15,15 @@ export const withRowLoadingPlaceholder = (
         const isLoaded = useDataTable(store => !!store.hasRowLoaded(props.index));
 
         const placeHolderRowStyle = useMemo(
-            () => [styles.placeHolderRow, props.style],
-            [props.style],
+            () => (isLoaded ? [styles.placeHolderRow, props.style] : undefined),
+            [props.style, isLoaded],
         );
 
-        if (!isLoaded) return <View style={placeHolderRowStyle} ref={ref} />;
-
-        return <Component {...props} ref={ref} />;
+        return (
+            <View style={placeHolderRowStyle} ref={ref}>
+                {isLoaded ? <Component {...props} ref={ref} /> : null}
+            </View>
+        );
     });
 
     RowRenderer.displayName = (Component.displayName ?? '') + 'WithPlaceholder';
