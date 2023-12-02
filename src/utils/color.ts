f164f533bed6fee04978e2571262bdb1033e67b9
elvisduru <duruelvis95@gmail.com>
2023-12-02 22:10:12 +0300

    chore: memoize

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,11 +1,10 @@
 import color from 'color';
+import { weakMemoized } from './lodash';
 
-export const resolveContrastColor = (
-    bgColor: string,
-    lightColor: string = '#fff',
-    darkColor: string = '#000',
-) => {
-    return color(bgColor).isLight() ? darkColor : lightColor;
-};
+export const resolveContrastColor = weakMemoized(
+    (bgColor: string, lightColor: string = '#fff', darkColor: string = '#000') => {
+        return color(bgColor).isLight() ? darkColor : lightColor;
+    },
+);
 
 export default color;
