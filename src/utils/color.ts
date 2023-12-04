81a65a49b34070627c3299dba89ea5561ad5e4b6
elvisduru <duruelvis95@gmail.com>
2023-12-04 17:30:31 +0300

    improvements: fallback when color is unresolvable

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -3,7 +3,11 @@ import { weakMemoized } from './lodash';
 
 export const resolveContrastColor = weakMemoized(
     (bgColor: string, lightColor: string = '#fff', darkColor: string = '#000') => {
-        return color(bgColor).isLight() ? darkColor : lightColor;
+        try {
+            return color(bgColor).isLight() ? darkColor : lightColor;
+        } catch {
+            return bgColor;
+        }
     },
 );
 
