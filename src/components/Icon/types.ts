31c85f28c4a02d579bbd6ccfc50dc15c5874efbe
Shashank Agarwal <shashank1010@gmail.com>
2023-12-06 13:46:54 +0300

    Fix IconProps

--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -1,5 +1,6 @@
-import type { IconProps as VectorIconProps } from 'react-native-vector-icons/Icon';
+import type { TextProps } from '@bambooapp/bamboo-atoms';
 import type { RefAttributes } from 'react';
+import type { ColorValue } from 'react-native';
 
 export enum IconPacks {
     Material = 'material',
@@ -16,6 +17,29 @@ export enum IconPacks {
     Entypo = 'entypo',
     AntDesign = 'antdesign',
     FontAwesome5 = 'font-awesome-5',
+}
+
+interface VectorIconProps extends TextProps {
+    /**
+     * Size of the icon, can also be passed as fontSize in the style object.
+     *
+     * @default 12
+     */
+    size?: number | undefined;
+
+    /**
+     * Name of the icon to show
+     *
+     * See Icon Explorer app
+     * {@link https://github.com/oblador/react-native-vector-icons/tree/master/Examples/IconExplorer}
+     */
+    name: string;
+
+    /**
+     * Color of the icon
+     *
+     */
+    color?: ColorValue | number | undefined;
 }
 
 export type IconType = `${IconPacks}`; // in TS 4.1+, we can do this to make enum values as a union type
